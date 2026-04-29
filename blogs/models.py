a0e7c78bd681ab38e330c487b33502d99d85099e
ahasanhabibmallick <ahasanhabibmallick@gmail.com>
2026-04-30 01:45:04 +0530

    Remove generated static files &posts by category

--- blogs/models.py
+++ blogs/models.py
@@ -23,10 +23,10 @@ STATUS_CHOICES =(
 class Blog(models.Model):
         title = models.CharField(max_length=200)
         slug = models.SlugField(max_length=150, unique=True, blank=True)
-        Category = models.ForeignKey(Category, on_delete=models.CASCADE, max_length=100)
+        category = models.ForeignKey(Category, on_delete=models.CASCADE, max_length=100)
         author = models.ForeignKey(User , on_delete=models.CASCADE)
         featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
-        short_dscription = models.TextField(max_length=500)
+        short_description = models.TextField(max_length=500)
         blog_body = models.TextField(max_length=2000)
         status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Draft")
         is_featured = models.BooleanField(default=False)
